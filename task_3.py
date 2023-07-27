34ea75281796839fd3dafa6c61adc4262ad06e2c
Anna <ankakras@yandex.ru>
2023-07-27 15:47:09 +0700

    update

--- task_3.py
+++ task_3.py
@@ -5,15 +5,15 @@
 TITLE = 'ТАБЛИЦА УМНОЖЕНИЯ'
 START = 2
 STOP = 10
-LAST_NUM_1 = int(STOP/2)
-LAST_NUM_2 = STOP - 1
+LAST_NUMBER_1 = int(STOP/2)
+LAST_NUMBER_2 = STOP - 1
 number_1 = START
 number_2 = START
 
 print(f'\t\t{TITLE}\n')
 while number_2 <= STOP:
-    for number_1 in range(START, LAST_NUM_1 + 1):
-        if number_1 == LAST_NUM_1:
+    for number_1 in range(START, LAST_NUMBER_1 + 1):
+        if number_1 == LAST_NUMBER_1:
             print(f'{number_1} x {number_2} = {number_1 * number_2}\t')
 
         else:
@@ -25,8 +25,8 @@ print()
 
 number_2 = START
 while number_2 <= STOP:
-    for number_1 in range(LAST_NUM_1 + 1, STOP):
-        if number_1 == LAST_NUM_2:
+    for number_1 in range(LAST_NUMBER_1 + 1, STOP):
+        if number_1 == LAST_NUMBER_2:
             print(f'{number_1} x {number_2} = {number_1 * number_2}\t')
 
         else:
